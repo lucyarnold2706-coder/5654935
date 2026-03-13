eeab3712efa16f6d4b0853a6ebcb8150f4e15f54
lucyarnold2706-coder <lucy.arnold2706@gmail.com>
2026-03-13 11:57:54 +0100

    Add MainForm.cs for Weather Application

---
+++ MainForm.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Windows.Forms;
+
+namespace WeatherApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadWeatherData();
+        }
+
+        private void LoadWeatherData()
+        {
+            // Load weather data from the Mannheimer Stunde 2024
+            // ... Implementation goes here
+        }
+    }
+}
